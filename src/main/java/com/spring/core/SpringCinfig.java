caf041f77761a466922f280aaa14ddb6b7f8514b
Iana Kasimova <mikkimous555@gmail.com>
2017-08-23 12:59:28 +0300

    new changes

---
+++ src/main/java/com/spring/core/SpringCinfig.java
@@ -0,0 +1,7 @@
+package com.spring.core;
+
+/**
+ * Created by Iana_Kasimova on 8/23/2017.
+ */
+public class SpringCinfig {
+}
---
+++ src/main/java/com/spring/core/beans/EventType.java
@@ -0,0 +1,8 @@
+package com.spring.core.beans;
+
+/**
+ * Created by Iana_Kasimova on 8/22/2017.
+ */
+public enum EventType {
+    INFO, ERROR
+}
